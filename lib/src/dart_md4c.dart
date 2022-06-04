efcce34a2d89fb8219bd80ac243a6bb695995c5b
laurensnol <laurensn@t-online.de>
2022-06-04 04:12:48 +0200

    Moved bindings to /src and created main library file

---
+++ lib/src/dart_md4c.dart
@@ -0,0 +1,108 @@
+import 'dart:ffi';
+import 'dart:io';
+
+// Unused right now
+// import 'dart:async';
+// import 'dart:isolate';
+
+import 'dart_md4c_bindings.dart';
+
+const String _libName = 'md4c';
+
+/// The dynamic library in which the symbols for [DartMd4cBindings] can be found.
+final DynamicLibrary _dylib = () {
+  if (Platform.isMacOS || Platform.isIOS) {
+    return DynamicLibrary.open('$_libName.framework/$_libName');
+  }
+  if (Platform.isAndroid || Platform.isLinux) {
+    return DynamicLibrary.open('lib$_libName.so');
+  }
+  if (Platform.isWindows) {
+    return DynamicLibrary.open('$_libName.dll');
+  }
+  throw UnsupportedError('Unknown platform: ${Platform.operatingSystem}');
+}();
+
+/// The bindings to the native functions in [_dylib].
+final DartMd4cBindings _bindings = DartMd4cBindings(_dylib);
+
+/*
+Note: Keeping this for later reference.
+
+/// A request to compute `sum`.
+///
+/// Typically sent from one isolate to another.
+class _SumRequest {
+  final int id;
+  final int a;
+  final int b;
+
+  const _SumRequest(this.id, this.a, this.b);
+}
+
+/// A response with the result of `sum`.
+///
+/// Typically sent from one isolate to another.
+class _SumResponse {
+  final int id;
+  final int result;
+
+  const _SumResponse(this.id, this.result);
+}
+
+/// Counter to identify [_SumRequest]s and [_SumResponse]s.
+int _nextSumRequestId = 0;
+
+/// Mapping from [_SumRequest] `id`s to the completers corresponding to the correct future of the pending request.
+final Map<int, Completer<int>> _sumRequests = <int, Completer<int>>{};
+
+/// The SendPort belonging to the helper isolate.
+Future<SendPort> _helperIsolateSendPort = () async {
+  // The helper isolate is going to send us back a SendPort, which we want to
+  // wait for.
+  final Completer<SendPort> completer = Completer<SendPort>();
+
+  // Receive port on the main isolate to receive messages from the helper.
+  // We receive two types of messages:
+  // 1. A port to send messages on.
+  // 2. Responses to requests we sent.
+  final ReceivePort receivePort = ReceivePort()
+    ..listen((dynamic data) {
+      if (data is SendPort) {
+        // The helper isolate sent us the port on which we can sent it requests.
+        completer.complete(data);
+        return;
+      }
+      if (data is _SumResponse) {
+        // The helper isolate sent us a response to a request we sent.
+        final Completer<int> completer = _sumRequests[data.id]!;
+        _sumRequests.remove(data.id);
+        completer.complete(data.result);
+        return;
+      }
+      throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
+    });
+
+  // Start the helper isolate.
+  await Isolate.spawn((SendPort sendPort) async {
+    final ReceivePort helperReceivePort = ReceivePort()
+      ..listen((dynamic data) {
+        // On the helper isolate listen to requests and respond to them.
+        if (data is _SumRequest) {
+          final int result = _bindings.sum_long_running(data.a, data.b);
+          final _SumResponse response = _SumResponse(data.id, result);
+          sendPort.send(response);
+          return;
+        }
+        throw UnsupportedError('Unsupported message type: ${data.runtimeType}');
+      });
+
+    // Send the the port to the main isolate on which we can receive requests.
+    sendPort.send(helperReceivePort.sendPort);
+  }, receivePort.sendPort);
+
+  // Wait until the helper isolate has sent us back the SendPort on which we
+  // can start sending requests.
+  return completer.future;
+}();
+*/
