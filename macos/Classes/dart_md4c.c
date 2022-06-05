d8be09e46fe14f3a57729bd4d1366131d11847d6
laurensnol <laurensn@t-online.de>
2022-06-05 15:42:47 +0200

    Updated platform specific build files

--- macos/Classes/dart_md4c.c
+++ macos/Classes/dart_md4c.c
@@ -1,3 +1,5 @@
 // Relative import to be able to reuse the C sources.
-// See the comment in ../{projectName}}.podspec for more information.
-#include "../../src/dart_md4c.c"
+// See the comment in ../dart_md4c.podspec for more information.
+#include "../../md4c/entity.c"
+#include "../../md4c/md4c.c"
+#include "../../md4c/md4c-html.c"
