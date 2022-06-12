f003a646cb46c2e2edead941d4e3356ee63b4bc3
laurensnol <laurensn@t-online.de>
2022-06-12 03:25:59 +0200

    Added additional exports

--- lib/dart_md4c.dart
+++ lib/dart_md4c.dart
@@ -1,2 +1,4 @@
 export 'src/dart_md4c.dart';
+export 'src/dart_md4c_bindings.dart' hide DartMd4cBindings;
 export 'src/dart_md4c_html.dart';
+export 'src/dart_md4c_html_bindings.dart' hide DartMd4cHtmlBindings;
